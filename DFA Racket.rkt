ae482387eb357baa5c9116dd0be71760a8580d93
mrboushy <mrboushy@users.noreply.github.com>
2020-03-31 10:05:02 -0700

    added transitions functions

--- DFA Racket.rkt
+++ DFA Racket.rkt
@@ -9,3 +9,26 @@
         )
     (b))
 )
+
+;;;transitions functions
+(define (a-state substring)
+    (cond
+        ((equal? substring "0") "a")
+        ((equal? substring "1") "b")
+        (else "not on the alphabet")
+    )
+)
+(define (b-state substring)
+    (cond
+        ((equal? substring "0") "c")
+        ((equal? substring "1") "a")
+        (else "not on the alphabet")
+    )
+)
+(define (c-state substring)
+    (cond
+        ((equal? substring "0") "b")
+        ((equal? substring "1") "c")
+        (else "not on the alphabet")
+    )
+)
